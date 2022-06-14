9c94c940c0805d9d562b6c542495b88bdf8e9065
Ebenezer <you@example.com>
2022-06-14 15:30:30 -0700

    Added command for 4-empty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * reset_to_98 - takes a pointer to an int
  *as a parameter and updates a value to 98
  * @n: parameter
  * Return: Always 0.
