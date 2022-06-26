a9667e640910c5e6c662c404211aedb370f13995
Ebenezer <you@example.com>
2022-06-26 08:26:34 -0700

    done

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,4 @@
-#include "main.h"
-
-/**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
- */
-
-void _puts(char *str)
+void _puts(char *s)
 {
-	int i = 0;
-
-	while (str[i])
-	{
-		_putchar(str[i]);
-		iii++;
-	}
-	_putchar('\n');
+	return;
 }
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,18 +1,4 @@
-#include "main.h"
-/**
- *_abs - Check Holberton
- * @r: An integre input
- * Description: This function returns absolute value of a number
- * Return: Absolut value of number r
- */
-int _abs(int r)
+int _abs(int n)
 {
-	if (r >= 0)
-	{
-		return (r);
-	}
-	else
-	{
-		return (r * -1);
-	}
+	return (0);
 }
