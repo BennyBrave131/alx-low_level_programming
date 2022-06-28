36adbb04e23b0987a294eb7d232f8da8971ee5d6
Ebenezer <you@example.com>
2022-06-28 03:17:17 -0700

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,5 +14,20 @@ int main(int argc, char *argv[])
 
 	for (a = 1; a < argc; a++)
 	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] > '9' || argv[a][b] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
+	for (c = 1; c < argc; c++)
+	{
+		num = atoi(argv[c]);
+		addNum += num;
+	}
+	printf("%d\n", addNum);
+	return (0);
 }
